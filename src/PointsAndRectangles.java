dfe6c511efa8b5a2d9fb5d0f26a91f8990893529
lish <shuzhou.li@ssfs.org>
2019-09-27 12:47:11 -0400

    rec

---
+++ src/PointsAndRectangles.java
@@ -0,0 +1,22 @@
+//Shirley li 9/27 practice for instantiation
+import java.awt.*;
+import java.util.*;
+public class PointsAndRectangles {
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter the x coordinate:");
+        int xcoord = input.nextInt();
+        System.out.print("Enter the y coordinate:");
+        int ycoord = input.nextInt();
+        System.out.print("Enter the length:");
+        int length = input.nextInt();
+        System.out.print("Enter the width:");
+        int width = input.nextInt();
+        Point point = new Point(xcoord,ycoord);
+        Dimension object = new Dimension(length,width);
+        Rectangle rec = new Rectangle(point,object);
+        System.out.println(point);
+        System.out.println(object);
+        System.out.println(rec);
+    }
+}
