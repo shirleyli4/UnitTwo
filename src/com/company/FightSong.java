51cff0d2677869018b8a7e06616bb94f42058d5f
lish <shuzhou.li@ssfs.org>
2019-10-07 11:17:19 -0400

    rec

---
+++ src/com/company/FightSong.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class FightSong {
+    public static void main(String[] args) {
+       goit();
+       space();
+       goit();
+       youwest();
+       goit();
+       space();
+        goit();
+        youwest();
+        goit();
+        space();
+        goit();
+    }
+       private static void goit(){
+            System.out.println("Go, team, go!\n" +
+                    "You can do it.");
+        }
+        private static void youwest (){
+            System.out.println("You're the best,\n" +
+                    "In the West.");
+        }
+        private static void space(){
+            System.out.println("");
+        }
+    }
+
