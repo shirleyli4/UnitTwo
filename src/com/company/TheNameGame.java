93c0a10ff954f2bb0cdcef8c6b4d1b1126185a51
lish <shuzhou.li@ssfs.org>
2019-10-09 13:51:21 -0400

    name

---
+++ src/com/company/TheNameGame.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class TheNameGame {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String first = input.nextLine();
+        System.out.println("What is your last name?");
+        String last = input.nextLine();
+
+    }
+
+}
