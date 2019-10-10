bf0c42ae8da4bea431ad70f34db5f6cc9bac9bb4
lish <shuzhou.li@ssfs.org>
2019-10-10 14:49:09 -0400

    wrapper

---
+++ src/com/company/WrappersDelight.java
@@ -0,0 +1,18 @@
+////Shirley Li 10/10/19
+package com.company;
+
+public class WrappersDelight {
+    public static void main(String[] args) {
+        int num1 = 14;
+        Integer num2 = 8;
+        double num3 = 2.5;
+        Double num4 = num2 + num3;
+        num2 = 5;
+        num3 = num1+num2+num4;
+        num1 = (int)(num2 +num4);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
