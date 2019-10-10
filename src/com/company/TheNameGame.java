071831d3cbbae0190832aadd2264ab9e9fc671fd
lish <shuzhou.li@ssfs.org>
2019-10-10 14:37:42 -0400

    the name game

--- src/com/company/TheNameGame.java
+++ src/com/company/TheNameGame.java
@@ -7,7 +7,15 @@ public class TheNameGame {
         String first = input.nextLine();
         System.out.println("What is your last name?");
         String last = input.nextLine();
-
+        lyric(first);
+        lyric(last);
+    }
+    private static void lyric (String a){
+        System.out.println(a+" "+a+", bo-B"+ a.substring(1));
+        System.out.println("Banana-fana fo-F"+a.substring(1));
+        System.out.println("Fee-fi-mo-M"+a.substring(1));
+        System.out.println(a.toUpperCase()+"!");
+        System.out.println("");
     }
 
 }
