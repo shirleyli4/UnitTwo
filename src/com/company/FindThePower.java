145b20953539fd070a6c165fa0574103eadba7af
lish <shuzhou.li@ssfs.org>
2019-10-22 14:04:43 -0400

    wrapper

--- src/com/company/FindThePower.java
+++ src/com/company/FindThePower.java
@@ -1,4 +1,12 @@
 package com.company;
 import java.util.Scanner;
 public class FindThePower {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the base: ");
+        double base = input.nextDouble();
+        System.out.println("Enter the exponent: ");
+        double exp = input.nextDouble();
+        System.out.println(base+"^"+exp+" = "+ Math.pow(base,exp));
+    }
 }
