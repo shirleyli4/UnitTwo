4fd555bd2266174e1f8556d089dfff5b0bb65a36
lish <shuzhou.li@ssfs.org>
2019-10-22 13:59:15 -0400

    wrapper

--- src/com/company/Courses.java
+++ src/com/company/Courses.java
@@ -2,11 +2,14 @@ package com.company;
 import java.util.Scanner;
 public class Courses {
     public static void main(String[]args){
-        Scanner input = new Scanner (System.in);
-        System.out.print("What is the description of the course?");
-        String answer = input.next();
-
-        System.out.println("Department: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the description of the course? ");
+        String des=input.nextLine();
+        int s1= des.indexOf(" ");
+        int s2= des.indexOf(" ",s1+1);
+        System.out.println("Department: "+des.substring(0,s1));
+        System.out.println("Course Number:"+des.substring(s1,s2+1));
+        System.out.println("Title: "+des.substring(s2+1));
     }
 
 }
---
+++ src/com/company/FindThePower.java
@@ -0,0 +1,4 @@
+package com.company;
+import java.util.Scanner;
+public class FindThePower {
+}
